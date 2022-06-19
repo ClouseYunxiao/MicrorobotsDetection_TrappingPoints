361354e589805e388866e06dbc6b6df0aa69bd05
Yunxiao Ren <67178826+ClouseYunxiao@users.noreply.github.com>
2022-06-19 14:53:01 +0100

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,7 +6,7 @@ image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 cv2.imshow("Original",image)
 cv2.waitKey()
 
-#拉普拉斯边缘检测
+
 lap = cv2.Laplacian(image,cv2.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv2.imshow("Laplacian",lap)
