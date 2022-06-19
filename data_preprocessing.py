694bacf7132fd4c5d96d773c2b579e699218e357
Yunxiao Ren <67178826+ClouseYunxiao@users.noreply.github.com>
2022-06-19 14:51:21 +0100

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+image = cv2.imread("H:\\img\\lena.jpg")
+image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original",image)
+cv2.waitKey()
+
+#拉普拉斯边缘检测
+lap = cv2.Laplacian(image,cv2.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv2.imshow("Laplacian",lap)
+cv2.waitKey()
